f32633d797193fe3017d78e4e30cff87cfb65694
Jolaterreureu <jonathancalvin.hessler@student.unisg.ch>
2026-03-06 18:00:21 +0100

    chagnes made

--- st_example.py
+++ st_example.py
@@ -6,4 +6,5 @@ st.title("This is my first streamlit project")
 st.header("Its about cars!")
 
 if st.button("Do you like cars"):
-    st.text("CARS " * 1000)
+    st.text("CARS " * 10)
+    st.text("YEAHH")
